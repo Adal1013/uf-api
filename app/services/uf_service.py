4b68bf6e654fec7343963996dbd0f270b53a646b
Adal <adalberto.avila@activeone.co>
2023-04-19 23:14:09 -0500

    add service class for get uf data types

--- app/services/uf_service.py
+++ app/services/uf_service.py
@@ -1,2 +1,16 @@
-async def home():
-    return { "data": "probando estructura" }
+from app.utils.uf_web_scraper import SIIUFWebScraper
+from fastapi import HTTPException
+
+class UFService:
+    @staticmethod
+    def get_ufs_data_by_year(year):
+        uf_web_scraper = SIIUFWebScraper(year)
+        return uf_web_scraper.get_uf_data()
+
+    @staticmethod
+    def get_uf_data_by_date(date):
+        uf_data = UFService.get_ufs_data_by_year(date.year)
+        try:
+            return uf_data[date.strftime("%Y-%m-%d")]
+        except KeyError:
+            raise HTTPException(status_code=404, detail="There is no fomento unit for that date.")

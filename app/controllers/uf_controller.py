dc4ca74c8bddff34dd40f5c88677be83bd0e0323
Adal <adalberto.avila@activeone.co>
2023-04-19 23:15:50 -0500

    fix in validate year call

--- app/controllers/uf_controller.py
+++ app/controllers/uf_controller.py
@@ -13,8 +13,8 @@ class UFController:
         return UFService.get_ufs_data_by_year(today.year)
 
     @staticmethod
-    def get_ufs(year: str = Depends(Validate.validate_year)):
-        return UFService.get_ufs_data_by_year(year)
+    def get_ufs(uf_year: str = Depends(Validate.validate_year)):
+        return UFService.get_ufs_data_by_year(uf_year)
 
     @staticmethod
     def get_uf(uf_date: str = Depends(Validate.validate_date)):
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -4,6 +4,6 @@ from app.controllers.uf_controller import UFController
 router = APIRouter()
 
 router.add_api_route(path='/last_ufs', endpoint=UFController.get_last_ufs, summary="Show the list of fomento unit for current year")
-router.add_api_route(path='/ufs/{year}', endpoint=UFController.get_ufs, summary="Show the list of fomento unit for a specific year")
+router.add_api_route(path='/ufs/{uf_year}', endpoint=UFController.get_ufs, summary="Show the list of fomento unit for a specific year")
 router.add_api_route(path='/uf/{uf_date}', endpoint=UFController.get_uf, summary="Show the fomento unit for a specific date")
 

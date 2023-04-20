7a7ee17d021538da5cc74ae926d1b600d3337848
Adal <adalberto.avila@activeone.co>
2023-04-19 23:15:07 -0500

    add endpoints to api

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.services.uf_service import home
+from app.controllers.uf_controller import UFController
 
 router = APIRouter()
 
-@router.get("/")
-async def read_root():
-    return await home()
+router.add_api_route(path='/last_ufs', endpoint=UFController.get_last_ufs, summary="Show the list of fomento unit for current year")
+router.add_api_route(path='/ufs/{year}', endpoint=UFController.get_ufs, summary="Show the list of fomento unit for a specific year")
+router.add_api_route(path='/uf/{uf_date}', endpoint=UFController.get_uf, summary="Show the fomento unit for a specific date")
+

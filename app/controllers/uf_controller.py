cd390d1601f1b74f8bfe73b1ce56d905eb96dbd6
Adal <adalberto.avila@activeone.co>
2023-04-19 23:14:50 -0500

    add controlle class with methods for endpoints

---
+++ app/controllers/uf_controller.py
@@ -0,0 +1,21 @@
+from fastapi import Depends
+from app.services.uf_service import UFService
+from app.utils.validate import Validate
+import datetime
+
+class UFController:
+    def __init__(self):
+        self.uf_service = UFService()
+
+    @staticmethod
+    def get_last_ufs():
+        today = datetime.date.today()
+        return UFService.get_ufs_data_by_year(today.year)
+
+    @staticmethod
+    def get_ufs(year: str = Depends(Validate.validate_year)):
+        return UFService.get_ufs_data_by_year(year)
+
+    @staticmethod
+    def get_uf(uf_date: str = Depends(Validate.validate_date)):
+        return UFService.get_uf_data_by_date(uf_date)
